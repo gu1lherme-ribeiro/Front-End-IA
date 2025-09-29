5dba4adf788ef6ae9204eac1461a2ac87ee37a55
Guilherme Fernandes <gfernandes@promagroup.com>
2025-09-29 00:03:51 -0300

    Initial commit - Projeto Front-End com interface de chat IA

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let circulo = document.querySelector(".circulo")
+let imagemCopo = document.querySelector(".imagem_copo")
+
+
+function mudarCor(cor) {
+    circulo.style.backgroundColor = cor
+}
+
+function trocaImagem (imagem){
+    imagemCopo.src = "img/img2.png"
+}
